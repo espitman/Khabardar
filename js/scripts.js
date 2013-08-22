ae78ccb353eb0d971deb71745eea7e24bca796c4
Saeed Heidari <saeed.heidari86@gmail.com>
2013-08-22 13:10:31 +0430

    0.0.008

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,2 @@
-Cufon.replace('.cufon', {onBeforeReplace:Bifon.convert});
+Cufon.replace('.cufon_yekan', {fontFamily: 'Yekan' ,onBeforeReplace:Bifon.convert});
+Cufon.replace('.cufon_icons', {fontFamily: 'Typicons'});
